900b426e4cb0db2a5fd0087ffd969b1ebd7e29e7
OthmanImam <imkargi@gmail.com>
2025-02-23 06:03:26 -0800

    Generated Sign-In.Service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { SignInService } from './sign-in.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, SignInService],
 })
 export class AuthModule {}
