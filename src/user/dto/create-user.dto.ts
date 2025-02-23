8c5baf5dee5a633ac383d68adc1174cf8d200da3
phertyameen <aminubabafatima8@gmail.com>
2025-02-23 21:01:15 +0100

    revert create-user-dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,28 @@
-export class CreateUserDto {}
+import { IsString, IsEmail, IsOptional, IsBoolean, IsUUID } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  password: string;
+
+  @IsOptional()
+  @IsString()
+  profilePicture?: string;
+
+  @IsBoolean()
+  @IsOptional()
+  isGuest?: boolean;
+
+  @IsOptional()
+  @IsString()
+  subscriptionTier?: string;
+
+  @IsOptional()
+  @IsString()
+  walletAddress?: string;
+}
