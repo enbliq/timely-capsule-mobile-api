caad7eadcbaca8f9064d559bd6a9a1d7046569b5
Shukazuby <shukazuby@gmail.com>
2025-02-23 13:47:57 +0100

    handled Transaction Modules and Dependencies

--- src/transaction/transaction.service.ts
+++ src/transaction/transaction.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { UserService } from 'src/user/user.service';
+import { CapsuleService } from 'src/capsule/capsule.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Transaction } from './entities/transaction.entity';
+import { Repository } from 'typeorm';
+
 
 @Injectable()
 export class TransactionService {
+  constructor(
+    private readonly userService: UserService,
+    private readonly capsuleService: CapsuleService,
+    @InjectRepository(Transaction) private readonly transactionRepository: Repository<Transaction>,
+  ) {}
+  
   create(createTransactionDto: CreateTransactionDto) {
     return 'This action adds a new transaction';
   }
