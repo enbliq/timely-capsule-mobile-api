b491bf86c7e253a4da3b70c4b6bb04af9f076038
Shukazuby <shukazuby@gmail.com>
2025-02-23 13:21:55 +0100

    updated app module with ConfigModule and TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,27 @@ import { AuthModule } from './auth/auth.module';
 import { CapsuleModule } from './capsule/capsule.module';
 import { GuestModule } from './guest/guest.module';
 import { TransactionModule } from './transaction/transaction.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entities/user.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
-  imports: [UserModule, AuthModule, CapsuleModule, GuestModule, TransactionModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        entities: [User],
+        synchronize: true,
+      }),
+    }),
+    UserModule, AuthModule, CapsuleModule, GuestModule, TransactionModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
