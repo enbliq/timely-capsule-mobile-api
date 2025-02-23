ab7f97067916a712be05940e2dcc70edfed03481
OthmanImam <imkargi@gmail.com>
2025-02-23 06:07:06 -0800

    Created a findOneByEmail method in UserService

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ){}
 
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
@@ -27,4 +32,11 @@ export class UserService {
     return `This action removes a #${id} user`;
   }
 
+  async findOneByEmail(email: string): Promise<User> {
+    const user = await this.userRepository.findOne({ where: { email } })
+    if (!user) {
+      throw new NotFoundException(`User with email ${email} not found`)
+    }
+    return user
+  }
 }
