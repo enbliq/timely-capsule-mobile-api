fa76120e45b122980ef944e9b8f002eb6c5a823b
OthmanImam <imkargi@gmail.com>
2025-02-23 06:21:02 -0800

    Updated Sign-In.Service

--- src/auth/sign-in.service.ts
+++ src/auth/sign-in.service.ts
@@ -1,6 +1,5 @@
 import { Injectable, UnauthorizedException } from "@nestjs/common"
 import type { UserService } from "../user/user.service"
-// import type { HashProvider } from "../providers/hash.provider"
 import type { SignInDto } from "./dto/sign-In.dto"
 import { JwtService } from "@nestjs/jwt"
 
@@ -10,7 +9,7 @@ export class SignInService {
     private userService: UserService,
     private jwtService: JwtService,
     //Waiting for HashingProvider to be implemented
-    // private hashProvider: HashProvider,
+
   ) {}
 
   async signIn(signInDto: SignInDto): Promise<{ access_token: string }> {
@@ -19,11 +18,7 @@ export class SignInService {
     try {
       const user = await this.userService.findOneByEmail(email)
 
-    //   const isPasswordValid = await this.hashProvider.compare(password, user.password)
-
-    //   if (!isPasswordValid) {
-    //     throw new UnauthorizedException("Invalid credentials")
-    //   }
+      // Waiting for HashingProvider to be implemented
 
       const payload = { sub: user.id, email: user.email }
       const access_token = this.jwtService.sign(payload)
