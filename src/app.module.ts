95bb4ade552d83472e9cb0a14e9e04d8ef90d3f7
Shukazuby <shukazuby@gmail.com>
2025-02-23 14:06:51 +0100

    removed app module implemetations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,32 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
-import { CapsuleModule } from './capsule/capsule.module';
-import { GuestModule } from './guest/guest.module';
-import { TransactionModule } from './transaction/transaction.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user/entities/user.entity';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import * as dotenv from 'dotenv';
-dotenv.config();
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        url: configService.get<string>('DATABASE_URL'),
-        entities: [User],
-        synchronize: true,
-      }),
-    }),
-    UserModule, AuthModule, CapsuleModule, GuestModule, TransactionModule
-  ],
   controllers: [AppController],
   providers: [AppService],
 })
