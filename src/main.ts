02c09a6482e446e6fa14cd143aab9756f14d8962
mdauwal <pricelezzchap@gmail.com>
2025-02-23 15:59:53 +0000

    corrected: implemented DTOs for User, Capsule, Guest Capsule Access Log & Transaction

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Enable global validation pipes
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Strips out properties that are not in the DTO
+      forbidNonWhitelisted: true, // Throws an error if unknown properties are sent
+      transform: true, // Transforms input to match DTO types (e.g., string to number)
+    }),
+  );
+
+  await app.listen(3000);
 }
 bootstrap();
--- src/transaction/dto/create-transaction.dto.ts
+++ src/transaction/dto/create-transaction.dto.ts
@@ -1,1 +1,34 @@
-export class CreateTransactionDto {}
+import { 
+    IsUUID, 
+    IsNumber, 
+    IsString, 
+    IsOptional, 
+    IsPositive 
+  } from 'class-validator';
+  
+  export class CreateTransactionDto {
+    @IsNumber()
+    @IsPositive()
+    amount: number;
+  
+    @IsString()
+    currency: string;
+  
+    @IsString()
+    status: string;
+  
+    @IsOptional()
+    @IsString()
+    transactionHash?: string;
+  
+    @IsString()
+    type: string;
+  
+    @IsUUID()
+    userId: string;
+  
+    @IsOptional()
+    @IsUUID()
+    capsuleId?: string;
+  }
+  
