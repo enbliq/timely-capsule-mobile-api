9df6cec7bb82318d27feb5da2eb9e118865fd41e
laryhills <hilaryokoh@gmail.com>
2025-02-24 12:48:27 +0100

    feat: :card_file_box: configure database with typeorm and updated sample db veriables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import databaseConfig from '../config/database.config';
+import databaseConfig, { validateConfig } from '../config/database.config';
+import appConfig from '../config/config';
+import * as crypto from 'crypto';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
@@ -10,24 +12,19 @@ import { TransactionModule } from './transaction/transaction.module';
 import { GuestModule } from './guest/guest.module';
 import { CapsuleModule } from './capsule/capsule.module';
 
+(global as any).crypto = crypto;
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: [`.env.${process.env.NODE_ENV || 'development'}`],
-      load: [databaseConfig],
+      load: [databaseConfig, appConfig],
+      validate: validateConfig,
     }),
     TypeOrmModule.forRootAsync({
       useFactory: (configService: ConfigService) => ({
-        type: 'postgres', // Add this line
-        host: configService.get('PGHOST'),
-        port: configService.get('PGPORT'),
-        username: configService.get('DBUSER'),
-        password: configService.get('DBPASSWORD'),
-        database: configService.get('PGDATABASE'),
-        // url: configService.get('DATABASE_URL'),
-        synchronize: configService.get('NODE_ENV') !== 'production',
-        autoLoadEntities: true,
+        ...configService.get('database'),
       }),
       inject: [ConfigService],
     }),
