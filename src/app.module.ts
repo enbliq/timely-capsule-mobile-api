580183a9106ed40aa9cf0b38078b81dc0bdd4951
Abdulmalik A. <amalikabdulmalik04@gmail.com>
2025-02-25 09:48:42 +0100

    "fix: database issue"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import databaseConfig, { validateConfig } from '../config/database.config';
 import appConfig from '../config/config';
-import * as crypto from 'crypto';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
@@ -14,21 +12,29 @@ import { CapsuleModule } from './capsule/capsule.module';
 import { PaginationModule } from './common/pagination/pagination.module';
 import { AdminModule } from './admin/admin.module';
 
-(global as any).crypto = crypto;
-
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: [`.env.${process.env.NODE_ENV || 'development'}`],
-      load: [databaseConfig, appConfig],
-      validate: validateConfig,
+      load: [appConfig],
     }),
-    TypeOrmModule.forRootAsync({
-      useFactory: (configService: ConfigService) => ({
-        ...configService.get('database'),
-      }),
-      inject: [ConfigService],
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DB_URL,
+      synchronize: true,
+      autoLoadEntities: true,
+      // useFactory: (configService: ConfigService) => ({
+      //   type: 'postgres', // Add this line
+      //   // host: configService.get('DB_HOST'),
+      //   // port: configService.get('DB_PORT'),
+      //   // username: configService.get('DB_USER'),
+      //   // password: configService.get('DB_PASSWORD'),
+      //   // database: configService.get('DB_NAME'),
+      //   url: configService.get('DB_URL'),
+      //   synchronize: true,
+      //   autoLoadEntities: true,
+      // }),
     }),
     UserModule,
     AuthModule,
