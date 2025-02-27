e23368c67343503db8a50b71e06a2d83a60811d1
Ahmed <ahmedmahmud1994@hotmail.com>
2025-02-27 11:31:51 +0100

    error handling and custom exception filters implementation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { GlobalExceptionFilter } from './common/exceptions/filters/global-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,9 @@ async function bootstrap() {
       forbidNonWhitelisted: true, // Throws an error if unknown properties are sent
       transform: true, // Transforms input to match DTO types (e.g., string to number)
     }),
+
+    // Apply the global exception filter
+    new GlobalExceptionFilter(),
   );
 
   await app.listen(3000);
