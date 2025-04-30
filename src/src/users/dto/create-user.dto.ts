dd11abe4c5ebb73888c75ba8c16166be89757519
BigBen7 <benardsimon7@gmail.com>
2025-04-30 13:03:26 +0100

    added the user resource

---
+++ src/src/users/dto/create-user.dto.ts
@@ -0,0 +1,26 @@
+import { IsEmail, IsNotEmpty, IsOptional, IsString, MinLength } from "class-validator"
+import type { AuthProvider } from "../schemas/user.schema"
+
+export class CreateUserDto {
+  @IsEmail()
+  @IsOptional()
+  email?: string
+
+  @IsString()
+  @MinLength(6)
+  @IsOptional()
+  password?: string
+
+  @IsString()
+  @IsNotEmpty()
+  displayName: string
+
+  @IsOptional()
+  roles?: string[]
+
+  @IsOptional()
+  guest?: boolean
+
+  @IsOptional()
+  provider?: AuthProvider
+}
---
+++ src/src/users/schemas/user.schema.ts
@@ -0,0 +1,41 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose"
+import type { Document } from "mongoose"
+
+export type UserDocument = User & Document
+
+export type AuthProvider = "local" | "google" | "github" | null
+
+@Schema({ timestamps: true })
+export class User {
+  @Prop({ required: false, unique: true, sparse: true })
+  email: string | null
+
+  @Prop({ required: false })
+  passwordHash: string | null
+
+  @Prop({ required: true })
+  displayName: string
+
+  @Prop({ type: [String], default: ["user"] })
+  roles: string[]
+
+  @Prop({ default: false })
+  guest: boolean
+
+  @Prop({ default: false })
+  isVerified: boolean
+
+  @Prop({ type: String, enum: ["local", "google", "github", null], default: null })
+  provider: AuthProvider
+
+  @Prop({ type: Date, default: Date.now })
+  lastLoginAt: Date
+
+  @Prop()
+  createdAt: Date
+
+  @Prop()
+  updatedAt: Date
+}
+
+export const UserSchema = SchemaFactory.createForClass(User)
