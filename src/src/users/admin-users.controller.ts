2e0c77a61b4292f4f82ce7335de47916673f7575
Falade Yusuf <faladeyusuf54@gmail.com>
2025-04-30 14:48:39 +0100

    Implement a role-based access control system for securing routes.

---
+++ src/src/users/admin-users.controller.ts
@@ -0,0 +1,39 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from "@nestjs/common"
+import type { UsersService } from "./users.service"
+import type { CreateUserDto } from "./dto/create-user.dto"
+import type { UpdateUserDto } from "./dto/update-user.dto"
+import { Roles } from "../auth/decorators/roles.decorator"
+import { VerifiedGuard } from "../auth/guards/verified.guard"
+import { RolesGuard } from "../auth/guards/roles.guard"
+
+@Controller("admin/users")
+@UseGuards(VerifiedGuard, RolesGuard)
+@Roles("admin")
+export class AdminUsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll()
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(id);
+  }
+
+  @Patch(":id")
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(id, updateUserDto)
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(id);
+  }
+}
